64d58a6a3928ed4784d221f5cb63a8bfee8dc56d
GenkiSugiyama <sgym0318marcos@gmail.com>
2024-04-28 18:40:28 +0900

    learn dotnet UnitTest

---
+++ StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using UtilityLibraries;
+
+namespace StringLibraryTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void 大文字で始まる文字列はTrueを返すか()
+    {
+        string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+        foreach (string word in words)
+        {
+            bool result = word.StartsWithUpper();
+            Assert.IsTrue(result, string.Format("Expected for '{0}': true; Actual: {1}", word, result));
+        }
+    }
+
+    [TestMethod]
+    public void 小文字で始まる文字列はFalseを返すか()
+    {
+        // Tests that we expect to return false.
+        string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство", "1234", ".", ";", " " };
+        foreach (string word in words)
+        {
+            bool result = word.StartsWithUpper();
+            Assert.IsFalse(result, string.Format("Expected for '{0}': true; Actual: {1}", word, result));
+        }
+    }
+}
